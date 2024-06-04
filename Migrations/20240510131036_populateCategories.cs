5bc9021e059750d5731111354a8bb1b49d99c9ff
Paulo Ricardo M <pr741234@gmail.com>
2024-06-04 12:48:48 -0300

    feat: add migrations

---
+++ Migrations/20240510131036_populateCategories.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations;
+
+/// <inheritdoc />
+public partial class populateCategories : Migration
+{
+  /// <inheritdoc />
+  protected override void Up(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Bebidas', 'bebidas.jpg')");
+    migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Lanches', 'lanches.jpg')");
+    migrationBuilder.Sql("Insert into Categories(Name, ImageUrl) Values('Sobremesas', 'sobremesas.jpg')");
+  }
+
+  /// <inheritdoc />
+  protected override void Down(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.Sql("Delete from Categories");
+  }
+}
---
+++ Migrations/20240510131949_populateProducts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations;
+
+/// <inheritdoc />
+public partial class populateProducts : Migration
+{
+  /// <inheritdoc />
+  protected override void Up(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.Sql("Insert into Products(Name, Description, Price, ImageUrl, Stock, RegisterData, CategoryID) Values('Coca Cola Diet', 'Refrigerante de cola 350ml', '5.45', 'cocacola.jpg', 50, getdate(), 1)");
+  }
+
+  /// <inheritdoc />
+  protected override void Down(MigrationBuilder migrationBuilder)
+  {
+    migrationBuilder.Sql("Delete from Products");
+  }
+}
